11239d42b23fa89c21d773e338ba37a8ea747a19
vignes-potharaj <freelancer.nenu@gmail.com>
2025-11-20 23:34:29 +0530

    problem: sorting check using without early returns

--- arrays/array-sort.js
+++ arrays/array-sort.js
@@ -30,9 +30,7 @@ console.log(isSorted([]))
 function isSorted1(arr) {
     for(let i = 0; i < arr.length - 1;i++){
         let diff = (arr[i] - arr[i+1]);
-        if (!diff) {
-            continue;
-        }
+        if (!diff) continue;
         let sign = diff/Math.abs(diff);
         if (sign +1) {
             return false;
@@ -41,8 +39,51 @@ function isSorted1(arr) {
     }
     return true;
 }
-
+// time complexity -o(n)
+// space complexity -o(1)
 console.log(isSorted1([1,2,3,4]))
 console.log(isSorted1([1,3,2,4]))
 console.log(isSorted1([5]))
 console.log(isSorted1([]))
+
+// Constraint B:  No early return allowed
+// meaning:
+// You cannot return inside the loop
+// You must finish the entire loop
+// Then decide sorted/not-sorted at the end
+
+function isSorted2(arr) {
+    let islarger = true;
+    for (let i = 0; i < arr.length - 1; i++) {
+        let diff = arr[i] - arr[i + 1];
+        if (!diff) continue;
+        let sign = diff / Math.abs(diff);
+        if (sign + 1) {
+            islarger = false;
+            break;
+        }
+    }
+    return islarger;
+}
+
+// time complexity -o(n)
+// space complexity -o(1)
+console.log(isSorted2([1,2,3,4]))
+console.log(isSorted2([1,3,2,4]))
+console.log(isSorted2([5]))
+console.log(isSorted2([]))
+
+// Constraint C: Recursion
+// Now the challenge is:
+// Rewrite isSorted using recursion instead of loops
+// And still NO comparison operators (> < >= <= == ===)
+
+function isSorted3(arr) {
+    
+}
+// time complexity -o(n)
+// space complexity -o(1)
+console.log(isSorted3([1,2,3,4]))
+console.log(isSorted3([1,3,2,4]))
+console.log(isSorted3([5]))
+console.log(isSorted3([]))
