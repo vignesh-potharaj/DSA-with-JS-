33b70c760ecabb9f430f5e2ef9ec8685fc6d3d5b
vignes-potharaj <freelancer.nenu@gmail.com>
2025-12-04 14:51:13 +0530

    leet code problem number 1

--- arrays/array-prefix.ts
+++ arrays/array-prefix.ts
@@ -60,7 +60,7 @@ console.log(sum([1,1,1,1], 2, 3));
 
 function twoSum(arr: number[], target: number): [number, number] | null {
     if (arr.length <= 1) return null;
-    for (let i = 0; i < arr.length;i++) {
+    for (let i = 0; i < arr.length - 1;i++) {
         for (let j = i + 1; j < arr.length;j++) {
             if (arr[i] + arr[j] === target) {
                 return [i,j]
@@ -75,3 +75,28 @@ console.log(twoSum([2,7,11,15], 9));    // [0,1]
 console.log(twoSum([3,2,4], 6));        // [1,2]
 console.log(twoSum([3,3], 6));          // [0,1]
 console.log(twoSum([1,2,3], 7));        // null
+
+
+// leetcode #1
+
+
+function twoSum1(arr: number[], target: number): [number, number] | null {
+    if (arr.length < 2) return null;
+    const map = new Map<number, number>();
+
+    for (let i = 0; i < arr.length;i++) {
+        const complement: number = target - arr[i];
+        if(map.has(complement)) {
+            return [map.get(complement)!, i];
+        }
+        map.set(arr[i],i);
+    }
+    return null;
+}
+
+// time complexity O(n)
+// space complexity O(n)
+console.log(twoSum1([2,7,11,15], 9));    // [0,1]
+console.log(twoSum1([3,2,4], 6));        // [1,2]
+console.log(twoSum1([3,3], 6));          // [0,1]
+console.log(twoSum1([1,2,3], 7));        // null
---
+++ leetcode/1.ts
@@ -0,0 +1,22 @@
+// leetcode #1
+
+function twoSum1(arr: number[], target: number): [number, number] | null {
+    if (arr.length < 2) return null;
+    const map = new Map<number, number>();
+
+    for (let i = 0; i < arr.length;i++) {
+        const complement: number = target - arr[i];
+        if(map.has(complement)) {
+            return [map.get(complement)!, i];
+        }
+        map.set(arr[i],i);
+    }
+    return null;
+}
+
+// time complexity O(n)
+// space complexity O(n)
+console.log(twoSum1([2,7,11,15], 9));    // [0,1]
+console.log(twoSum1([3,2,4], 6));        // [1,2]
+console.log(twoSum1([3,3], 6));          // [0,1]
+console.log(twoSum1([1,2,3], 7));        // null
