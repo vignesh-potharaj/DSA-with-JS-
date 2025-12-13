1f7d00854c72afa80d5291c767427101d7a67e2b
vignes-potharaj <freelancer.nenu@gmail.com>
2025-12-13 14:08:02 +0530

    feat: find First Non-Repeating Element using map

--- js-basics/maps.ts
+++ js-basics/maps.ts
@@ -51,3 +51,31 @@ let B = [5, 7, 10]
 console.log(twoCommon(A, B));  
 
 
+// Problem 4: First Non-Repeating Element using map
+
+function firstUnq(arr: number[]): number | null {
+    let result: number = 0;
+    const map = new Map();
+    
+    for(const num of arr) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+
+    for(const num of arr) {
+        if(map.get(num) === 1) return num;
+    }
+    return null;
+}
+
+// THE PROBLEM IS WE NEED TO FIND OUT THE FIRST NON REPEATING NUMBER FROM AN ARRAY
+// - USING MAPS
+// -PASS ONLY AN ARRAY
+// - WE NEED TO RETURN A NUMBER
+// HOW ABOUT WE CREATE A AN ARRAY OF REPEATING NUMBERS AND RETURN THE FIRST ONE
+// -NOTE: IF WE CAN DO THIS WE CAN UPGRADE THE CODE TO DIRECTLY RETURN THE FIRST RETURNING VARIABLE
+// SO HOW DO WE DO CREATE AN ARRAY OF REPEATING ELEMENTS USING A MAP
+// THE QUESTION IS WHY AN ARRAY WHY NOT A MAP( WHY MAP=> ARRAY=> NUMBER WHEN MAP=>NUMBER IS POSSIBLE)
+// LETS CREATE A MAP WHERE ELEMENTS WHICHB ARE REPEATED ARE RPESENT
+
+
+console.log(firstUnq([4, 5, 1, 2, 0, 4, 1]));
