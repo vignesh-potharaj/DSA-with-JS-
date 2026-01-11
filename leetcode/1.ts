3e9390b9f6706b68678dd922be682413398411de
vignes-potharaj <freelancer.nenu@gmail.com>
2026-01-11 13:24:12 +0530

    revised leet code problem no 1: two sum

--- leetcode/1.ts
+++ leetcode/1.ts
@@ -20,3 +20,16 @@ console.log(twoSum1([2,7,11,15], 9));    // [0,1]
 console.log(twoSum1([3,2,4], 6));        // [1,2]
 console.log(twoSum1([3,3], 6));          // [0,1]
 console.log(twoSum1([1,2,3], 7));        // null
+function twoSum(nums: number[], target: number) {
+    if(nums.length <= 1) return null;
+    const map = new Map<number, number>();
+    for(let i = 0 ; nums.length > 1;i++) {
+        let complement: number = target - nums[i];
+        if(map.has(complement)) {
+            return [map.get(complement),i];
+        }
+        else {
+            map.set(nums[i],i);
+        }
+    }
+};
