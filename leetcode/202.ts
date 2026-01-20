7d77ac2a59fa4d39f3537e5a19bcf8c57a9287fb
vignes-potharaj <freelancer.nenu@gmail.com>
2026-01-20 23:37:11 +0530

    feat: solved leetcode problem number 202

---
+++ leetcode/202.ts
@@ -0,0 +1,12 @@
+function isHappy(n: number): boolean {
+    const seen = new Set<number>();
+    while(n !== 1) {
+        if(seen.has(n)) return false;
+        seen.add(n);
+        const digits = String(n).split('').map(Number);
+        n = digits.reduce((sum, num) => sum + (num*num),0);
+    }
+    return true;
+};
+// time - O(log)n
+// space - O(log)n
